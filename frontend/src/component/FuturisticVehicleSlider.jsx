d7f4b235855e67d080f740656d8b7f2ebf5d9adb
Aditya <adityayennam779@gmail.com>
2025-02-23 02:51:14 +0530

    vehicles added

--- frontend/src/component/FuturisticVehicleSlider.jsx
+++ frontend/src/component/FuturisticVehicleSlider.jsx
@@ -5,15 +5,31 @@ import s1 from "../assets/img/s1.png";
 import s2 from "../assets/img/s2.png";
 import s3 from "../assets/img/s3.png";
 import s4 from "../assets/img/S4.png";
+import s5 from "../assets/img/s5.png";
+import s6 from "../assets/img/s6.png";
+import s7 from "../assets/img/s7.png";
+import s8 from "../assets/img/s8.png";
+import c1 from "../assets/img/c1.png";
+import c2 from "../assets/img/c2.png";
+import c3 from "../assets/img/c3.png";
+import c4 from "../assets/img/c4.png";
+import c5 from "../assets/img/c5.png";
+import c6 from "../assets/img/c6.png";
+import c7 from "../assets/img/c7.png";
+import b1 from "../assets/img/b1.png";
+import b2 from "../assets/img/b2.png";
+import A1 from "../assets/img/A1.png";
+import A2 from "../assets/img/A2.png";
+import A3 from "../assets/img/A3.png";
 
 const vehicles = [
   {
     id: 1,
     name: "OLA",
     image: s1,
-    engine: "1200 CC",
-    speed: "450 KM/H",
-    power: "380 HP",
+    engine: "Electric Moter",
+    speed: "120 KPH",
+    power: "11 KW",
     category: "MotorBikes",
   },
   {
@@ -21,8 +37,8 @@ const vehicles = [
     name: "ATHER 450X",
     image: s2,
     engine: "Electric Motor",
-    speed: "250 KM/H",
-    power: "670 HP",
+    speed: "90 KPH",
+    power: "6.4 KW",
     category: "MotorBikes",
   },
   {
@@ -30,8 +46,8 @@ const vehicles = [
     name: "TVS IQUBE",
     image: s3,
     engine: "Electric",
-    speed: "320 KM/H",
-    power: "300 HP",
+    speed: "81 KPH",
+    power: "4.4 KW",
     category: "MotorBikes",
   },
   {
@@ -39,8 +55,151 @@ const vehicles = [
     name: "BAJAJ CHETAK",
     image: s4,
     engine: "Electric",
-    speed: "",
-    power: "300 HP",
+    speed: "73 KPH",
+    power: "4 KW",
+    category: "MotorBikes",
+  },
+  {
+    id: 5,
+    name: "TESLA",
+    image: c1,
+    engine: "Electric",
+    speed: "320 KPH",
+    power: "1020 HP",
+    category: "Cars",
+  },
+  {
+    id: 6,
+    name: "WHITE TESLA",
+    image: c2,
+    engine: "Electric",
+    speed: "320 KPH",
+    power: "1020 HP",
+    category: "Cars",
+  },
+  {
+    id: 7,
+    name: "Mersedes-Benz G 580",
+    image: c3,
+    engine: "Electric",
+    speed: "180 KPH",
+    power: "587 HP",
+    category: "Cars",
+  },
+  {
+    id: 8,
+    name: "Lamborgini Urus",
+    image: c4,
+    engine: "Electric",
+    speed: "312 KPH",
+    power: "588 KW",
+    category: "Cars",
+  },
+  {
+    id: 9,
+    name: "Mahendra XEV BE",
+    image: c5,
+    engine: "Electric",
+    speed: "202 KPH",
+    power: "228 HP",
+    category: "Cars",
+  },
+  {
+    id: 10,
+    name: "Tata Curvv Ev",
+    image: c6,
+    engine: "Electric",
+    speed: "160 KPH",
+    power: "55 KW",
+    category: "Cars",
+  },
+  {
+    id: 11,
+    name: "BMW i7",
+    image: c7,
+    engine: "Electric",
+    speed: "250 KPH",
+    power: "660 HP",
+    category: "Cars",
+  },
+  {
+    id: 12,
+    name: "Hummer Ev",
+    image: b1,
+    engine: "Electric",
+    speed: "45 KPH",
+    power: "1200 watts",
+    category: "Bicycles",
+  },
+  {
+    id: 13,
+    name: "STROOM!",
+    image: b2,
+    engine: "Electric",
+    speed: "45 KPH",
+    category: "Bicycles",
+  },
+  {
+    id: 14,
+    name: "Mahendra TREO",
+    image: A1,
+    engine: "Electric",
+    speed: "55 KPH",
+    power: "8 KW",
+    category: "AutoRickshaws",
+  },
+  {
+    id: 15,
+    name: "GK Rikshaws",
+    image: A2,
+    engine: "Electric",
+    speed: "25 KPH",
+    power: "1 HP",
+    category: "AutoRickshaws",
+  },
+  {
+    id: 16,
+    name: "Mahendra TREO",
+    image: A3,
+    engine: "Electric",
+    speed: "55 KPH",
+    power: "8 KW",
+    category: "AutoRickshaws",
+  },
+  {
+    id: 17,
+    name: "Hero Vida v1 Pro",
+    image: s5,
+    engine: "Electric",
+    speed: "80 KPH",
+    power: "5 kw",
+    category: "MotorBikes",
+  },
+  {
+    id: 18,
+    name: "Rorr Ez",
+    image: s6,
+    engine: "Electric",
+    speed: "95 KPH",
+    power: "7.5 kw",
+    category: "MotorBikes",
+  },
+  {
+    id: 19,
+    name: "Revolt Rv1",
+    image: s7,
+    engine: "Electric",
+    speed: "70 kph",
+    power: "2.8 KW",
+    category: "MotorBikes",
+  },
+  {
+    id: 20,
+    name: "OLA Roadster",
+    image: s8,
+    engine: "Electric",
+    speed: "125 KPH",
+    power: "11 KW",
     category: "MotorBikes",
   },
 ];
